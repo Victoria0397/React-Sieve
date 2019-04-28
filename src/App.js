efceebb08e53f76de14bbae355855aa819984800
Your Name <you@example.com>
2019-04-29 01:13:57 +0300

    New features

--- src/App.js
+++ src/App.js
@@ -4,17 +4,43 @@ import './App.css';
 import Prime from './Prime/Prime';
 
 class App extends Component {
-  sendToServer(){
-    fetch('https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5')
+  sendToServer(start, end){
+    fetch('http://localhost:80/?start='+start+'&end='+end+'')
       .then(function(response) {
-        console.log(response);
+        return response.json();
+      })
+      .then(function(data){
+        console.log(data);
       });
   }
+
+  state = {
+    start: 2,
+    end: 100
+  }
+
+  handleStartValue(event){
+    this.setState({
+      start: event.target.value
+    });
+  }
+
+  handleEndValue(event){
+    this.setState({
+      end: event.target.value
+    });
+  }
+
+  
+
   render(){
     
     return (
       <div className="App">
-        <Prime onSendToServer={this.sendToServer.bind(this)}/>
+        <h1>Its for test Eratosphene</h1>
+        <input type="text" onChange={this.handleStartValue.bind(this)}/>
+        <input type="text" onChange={this.handleEndValue.bind(this)}/>
+        <Prime onSendToServer={this.sendToServer.bind(this, this.state.start, this.state.end)}/>
       </div>
     );
   }
